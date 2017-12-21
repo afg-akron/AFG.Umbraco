8a28c39055cf5868874531640368f7b9dc79a56e
Richie Lipton <richard.lipton@gmail.com>
2017-12-20 23:29:31 -0500

    Add project files.

---
+++ AFG.Umbraco.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AFG.Umbraco.Web.Startup))]
+namespace AFG.Umbraco.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
